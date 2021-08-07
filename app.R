972d2681d9b7d117e70c274fb89d9208c406aa5e
MadwolfDT <davmes.tan.2020@mitb.smu.edu.sg>
2021-08-07 18:35:52 +0800

    Davmes MC 2

--- app.R
+++ app.R
@@ -10,10 +10,17 @@ rsconnect::setAccountInfo(name='dtcs', token='25A37523AE52220A0DE445A9D8B696DE',
 emply_data <- read_csv("data/car-assignments.csv")
 cc_data <- read_csv("data/cc_data.csv")
 loyalty_data <- read_csv("data/loyalty_data.csv")
+gps <- read_csv("data/gps.csv")
 
 ##################import MC 2 data into variables##############################
 
 ##########################cleaning MC 2 data###################################
+gps$Timestamp <- date_time_parse(gps$Timestamp,
+                                 zone = "",
+                                 format = "%m/%d/%Y %H:%M:%S")
+
+gps <- gps %>%
+  mutate(datestamp = as.Date(Timestamp + 60*60*8))
 
 #convert timestamp from character into date/time format
 cc_data$timestamp <- date_time_parse(cc_data$timestamp,
@@ -39,6 +46,13 @@ loyalty_data <- loyalty_data %>%
 
 ##########################cleaning MC 2 data###################################
 
+##########################DT Variables###################################
+
+gps_date <- gps %>%
+  distinct(datestamp)
+
+##########################DT Variables###################################
+
 # Define UI
 ui <- navbarPage(
   theme = shinytheme("superhero"),
@@ -61,17 +75,25 @@ ui <- navbarPage(
                                  
                                  inputId = "dtemployee_name",
                                  label = "Employee Name",
-                                 choices = c(emply_data$FirstName),
+                                 choices = c(paste(emply_data$FirstName, emply_data$LastName, sep = " ")),
+                                 
+                               ),
+                               
+                               selectInput(
+                                 
+                                 inputId = "dtdate",
+                                 label = "Date",
+                                 choices = c(gps_date$datestamp),
                                  
                                ),
                                
                                ),
-                        column(9,"Two"),
+                        column(9,"Plotted Map to be displayed here"),
                       ),
                       
                       fluidRow(
-                        column(6,"Three"),
-                        column(6,"Four"),
+                        column(6,"Selected Personnel Parking Details"),
+                        column(6,"Population Parking Details"),
                       ),
                       
                       ),
